eff730c5f293a2611923e21ace846a5aa426afe8
Mayank Badola <badola21295@gmail.com>
2016-06-10 14:46:44 +0530

    navigation template initialised

--- app/client/index.js
+++ app/client/index.js
@@ -10,6 +10,10 @@ class About extends Component {
     this.state = {}
   }
 
+  componentWillMount() {
+    this.props.navUpdate("About")
+  }
+
   render() {
     return (
       <p>About section! Go back to</p>
@@ -23,6 +27,10 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+  }
+
+  componentWillMount() {
+    this.props.navUpdate("Home")
   }
 
   render() {
@@ -42,6 +50,10 @@ class NoMatch extends Component {
     this.state = {}
   }
 
+  componentWillMount() {
+    this.props.navUpdate("404")
+  }
+
   render() {
     return (
       <p>Invalid scope! Go back to</p>
@@ -54,20 +66,43 @@ class Wrapper extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      "nav": {
+        "Home": true,
+        "About": false,
+        "404": false
+      }
+    }
+    this.changeRoute = this.changeRoute.bind(this)
+
+  }
+
+  changeRoute(newRoute) {
+    console.log(newRoute)
+    let home = newRoute === "Home" ? true : false
+    let about = newRoute === "About" ? true : false
+    let notFound = newRoute === "404" ? true : false
+    this.setState({
+      "nav": {
+        "Home": home,
+        "About": about,
+        "404": notFound
+      }
+    })
   }
 
   render() {
+    console.log("Main rendered")
     return (
       <div>
-        <div>
+        <div className="main-nav">
           <ul>
-            <li><Link to={`/`}>Home</Link></li>
-            <li><Link to={`/about`}>About</Link></li>
-            <li><Link to={`/not`}>404</Link></li>
+            <li className={ this.state.nav["Home"] ? `active` : `off` }><Link to={`/`}>Home</Link></li>
+            <li className={ this.state.nav["About"] ? `active` : `off` }><Link to={`/about`}>About</Link></li>
+            <li className={ this.state.nav["404"] ? `active` : `off` }><Link to={`/not`}>404</Link></li>
           </ul>
         </div>
-        { this.props.children }
+        { React.cloneElement(this.props.children, { navUpdate: this.changeRoute }) }
       </div>
     )
   }
