934235492464a9244d832c8db15a5b4ca5164da1
Samarth Chugh <samakha.chugh@gmail.com>
2016-07-06 13:49:40 +0530

    Needs bootstrap correction

--- app/client/components/AccCardComponents.js
+++ app/client/components/AccCardComponents.js
@@ -11,11 +11,13 @@ class AccCard extends Component{
 
 	render() {
 		let bgImg = {
-			backgroundImage: 'url(' + this.props.content.img + ')'
+			backgroundImage: 'url(' + this.props.content.img + ')',
+			width: '100%'
 		}
 
 		return (
-      <div className="mem-card col-md-4 col-lg-4">
+      <div className="mem-card col-md-4 col-lg-4 col-xs-8 col-centered">
+      	<figure>
 		    <Link className="img front" style={ bgImg } to={`/not`}></Link>
 			<div className="img back">
 				<span>{ this.props.content.name }</span>
@@ -24,13 +26,13 @@ class AccCard extends Component{
 			      {
 			        this.props.content.contact.map((r,s) => {
 			        	return this.props.content.contact_link.map((v,k) => {
-			        		return <a key={k} className="contact" href="v"><i key={s} className={`fa fa-${r}-square`} aria-hidden="true"></i></a>
+			        		return <a key={s} className="contact" href={v}><i key={s} className={`fa fa-${r}-square`} aria-hidden="true"></i></a>
 			        	})
 			        })
 			      }
 			    </div>
 			</div>
-		    
+		</figure>
       </div>
     )
   }
