73c6448acecab57a8c0f9b84c96bafd8780b7d94
Shubham Kumaram <krm.shubham@gmail.com>
2016-07-17 18:01:04 +0530

    Fixed responsiveness issue

--- app/client/components/AccCardComponents.js
+++ app/client/components/AccCardComponents.js
@@ -18,24 +18,25 @@ class AccCard extends Component{
 		return (
       <div className="mem-card col-sm-6 col-md-3 col-lg-3 col-xs-12">
       	<figure className="img-circle">
-		    <Link className="img front img-circle" style={ bgImg } to={`/not`}></Link>
-			<div className="img back img-circle">
-				<span>{ this.props.content.name }</span>
-				<span>{ this.props.content.post }</span>
-				<div className="share">
-            {
-              this.props.content.contact.map((r,s) => {
-                return this.props.content.contact_link.map((v,k) => {
-                if(s == k){
-                  return <a key={s} className="contact" href={v}><i key={k} className={`fa fa-${r}-square fa-lg`} aria-hidden="true"></i></a>
-                }
-                })
-              })
-            }
-        </div>
-
+		<Link className="img front img-circle" style={ bgImg } to={`/not`}></Link>
+		<div className="img back img-circle">
+			<div className="info">
+				<p>{ this.props.content.name }</p>
+				<p>{ this.props.content.post }</p>
 			</div>
-		</figure>
+			<div className="share">
+			    {
+			      this.props.content.contact.map((r,s) => {
+				return this.props.content.contact_link.map((v,k) => {
+				if(s == k){
+				  return <a key={s} className="contact" href={v}><i key={k} className={`fa fa-${r}-square fa-lg`} aria-hidden="true"></i></a>
+				}
+				})
+			      })
+			    }
+			</div>
+		</div>
+	</figure>
       </div>
     )
   }
