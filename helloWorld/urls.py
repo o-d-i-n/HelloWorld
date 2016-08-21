5cbb9c0adf3c87968a72da4d7c2ddc63f6bf8316
Mayank Badola <badola21295@gmail.com>
2016-08-21 11:22:00 +0530

    Media route workaround

--- helloWorld/urls.py
+++ helloWorld/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/',include('api.urls')),
-    url(r'^',include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^api/',include('api.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [url(r'^',include('app.urls'))]
