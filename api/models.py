2c68c8860d1b39f189f8020ea77f8450e10e825f
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-10-11 02:01:21 +0530

    models corrected | changed img field to url field

--- api/models.py
+++ api/models.py
@@ -5,88 +5,99 @@ from django_mysql.models import JSONField, Model
 from django.template.defaultfilters import slugify
 
 ##########################################################
-#MEMBERS
+# MEMBERS
 ##########################################################
 # {
-# 	name: "Abhinav Bansal",
-# 	post: "Collaborator",
-# 	img: "/media/mockups/cheater_rfgubPd.jpg",
-# 	contacts: [
-# 		{
-# 			name: "github",
-# 			link: "http://www.github.com/ab-decoded"
-# 		},
-# 		{
-# 			name: "facebook",
-# 			link: "http://facebook.com/abhinavbansal.1217"
-# 		}
-# 	]
+#   name: "Abhinav Bansal",
+#   post: "Collaborator",
+#   img: "https://avatars3.githubusercontent.com/u/10768588?v=3&s=466",
+#   contacts: [
+#       {
+#           name: "github",
+#           link: "http://www.github.com/ab-decoded"
+#       },
+#       {
+#           name: "facebook",
+#           link: "http://facebook.com/abhinavbansal.1217"
+#       }
+#   ]
 # }
+
 class Contact(models.Model):
-	name=models.CharField(max_length=50)
-	link=models.CharField(max_length=100)
-	def __str__(self):
-		return self.link+"("+self.name+")"
+    name = models.CharField(max_length=50)
+    link = models.CharField(max_length=100)
+
+    def __str__(self):
+        contact_string = "{0}({1})".format(self.link, self.name)
+        return contact_string
+
 class Member(models.Model):
-	name=models.CharField(max_length=50)
-	post=models.CharField(max_length=50)
-	img=models.ImageField(upload_to='mockups',null=True)
-	contacts=models.ManyToManyField(Contact)
-	def __str__(self):
-		return self.name+"("+self.post+")"
+    name = models.CharField(max_length=50)
+    post = models.CharField(max_length=50)
+    img = models.URLField(null=True)
+    contacts = models.ManyToManyField(Contact)
+
+    def __str__(self):
+        name_post_string = "{0}({1})".format(self.name, self.post)
+        return name_post_string
 
 
 ##########################################################
-#PROJECTS
+# PROJECTS
 ##########################################################
 # {
-# slug: "nyan-cat",
-# name: "Nyan Cat",
-# type: "Lol",
-# desc: "Short descrption",
-# icon: "/media/icons/cheater.jpg",
-# technologies: [
-# 	"Meteor",
-# 	"Mongo"
-# ],
-# long_desc: "Long description...",
-# contributors: [
-# 	 {
-#	 	name: "Abhinav Bansal",
-#	 	github: "ab-decoded"
-#	 }
-# ],
-# "meta": {
-# 	"updated": "20-Jul-2016",
-# 	"size": "3M",
-# 	"version": "1.0.0",
-# 	"platform": "All"
+#     slug: "nyan-cat",
+#     name: "Nyan Cat",
+#     type: "Lol",
+#     desc: "Short descrption",
+#     icon: "/media/icons/cheater.jpg",
+#     technologies: [
+#         "Meteor",
+#         "Mongo"
+#     ],
+#     long_desc: "Long description...",
+#     contributors: [
+#         {
+#             name: "Abhinav Bansal",
+#             github: "ab-decoded"
+#         }
+#     ],
+#     "meta": {
+#         "updated": "20-Jul-2016",
+#         "size": "3M",
+#         "version": "1.0.0",
+#         "platform": "All"
+#     }
 # }
-# }
+
 class Technology(models.Model):
-	name=models.CharField(max_length=50,primary_key=True)
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=50, primary_key=True)
+    
+    def __str__(self):
+        return self.name
 
 class Contributor(models.Model):
-	name=models.CharField(max_length=50)
-	github=models.CharField(max_length=50)
-	def __str__(self):
-		return self.name + "(" + self.github + ")"
+    name = models.CharField(max_length=50)
+    github = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name + "(" + self.github + ")"
 
 class Project(models.Model):
-	slug=models.CharField(max_length=100,blank=True,primary_key=True)
-	name=models.CharField(max_length=50)
-	type=models.CharField(max_length=50)
-	desc=models.TextField()
-	icon=models.ImageField(upload_to="icons",null=True)
-	technologies=models.ManyToManyField(Technology,blank=True)
-	long_desc=models.TextField(blank=True)
-	contributors=models.ManyToManyField(Contributor)
-	meta=JSONField()
-	def __str__(self):
-		return self.name+"("+self.desc+")"
-	def save(self,*args,**kwargs):
-		if not self.slug:
-			self.slug=slugify(self.name)
-		super(Project,self).save(*args,**kwargs)
+    slug = models.CharField(max_length=100, blank=True, primary_key=True)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    desc = models.TextField()
+    icon = models.ImageField(upload_to="icons", null=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
+    long_desc = models.TextField(blank=True)
+    contributors = models.ManyToManyField(Contributor)
+    meta = JSONField()
+
+    def __str__(self):
+        return self.name+"("+self.desc+")"
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Project,self).save(*args,**kwargs)
--- api/views.py
+++ api/views.py
@@ -2,30 +2,43 @@ from django.shortcuts import render
 from .models import Project,Member,Contact,Technology,Contributor
 from .serializers import MemberSerializer,ProjectSerializer
 from django.http import HttpResponse,Http404
+
 import json
+
+from utils import my_func
 
 # API wale endpoints
 def projects(request):
-	if request.method=="GET":
-		projects = ProjectSerializer(instance=Project.objects.all(),many=True)
-		return HttpResponse(json.dumps(projects.data))
-	else:
-		return HttpResponseForbidden('allowed only via GET')
+    if request.method == "GET":
+        projects = ProjectSerializer(instance=Project.objects.all(),many=True)
+        return HttpResponse(json.dumps(projects.data))
+    else:
+        return HttpResponseForbidden('allowed only via GET')
 
 def project_details(request,project_slug):
-	if request.method=="GET":
-		try:
-			project=Project.objects.get(slug=project_slug)
-			project_serialized = ProjectSerializer(instance=project)
-			return HttpResponse(json.dumps(project_serialized.data))
-		except Project.DoesNotExist:
-			return HttpResponse(json.dumps({'error':'No project matches the given query'}));
-	else:
-		return HttpResponseForbidden('allowed only via GET')
+    if request.method == "GET":
+        try:
+            project = Project.objects.get(slug=project_slug)
+            project_serialized = ProjectSerializer(instance=project)
+            
+            return HttpResponse(json.dumps(project_serialized.data))
+        except Project.DoesNotExist:
+            error_message = "No project matches the given query"
+            error_response = {
+                "error" : error_message,
+            }
+
+            return HttpResponse(json.dumps(error_response));
+    else:
+        return HttpResponseForbidden('allowed only via GET')
 
 def members(request):
-	if request.method=="GET":
-		members = MemberSerializer(instance = Member.objects.all(),many=True)
-		return 	HttpResponse(json.dumps(members.data))
-	else:
-		return HttpResponseForbidden('allowed only via GET')
+    if request.method == "GET":
+        members = MemberSerializer(instance=Member.objects.all(), many=True)
+        return HttpResponse(json.dumps(members.data))
+    else:
+        return HttpResponseForbidden('allowed only via GET')
+
+def test(request):
+	
+	return 	HttpResponse(json.dumps(my_func()))
